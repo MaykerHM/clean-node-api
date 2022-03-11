6124438d7477c45201312e8b307be2699bbf5708
MaykerHM <hydeki8@gmail.com>
2022-03-10 22:46:11 -0300

    refactor: coverage ignore protocols

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  // collectCoverage: true,
-  // coverageProvider: 'v8',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**',
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
